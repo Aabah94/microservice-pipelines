0b1802291738746eef8ea7498d569712b8b930cf
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2020-04-03 15:00:46 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -15,12 +15,12 @@ def call(){
 
 def packageArtifact(){
     stage("Package artifact") {
-        sh "mvn package"
+        bat "mvn package"
     }
 }
 
 def buildAndTest(){
     stage("Backend tests"){
-        sh "mvn test"
+        bat "mvn test"
     }
 }
