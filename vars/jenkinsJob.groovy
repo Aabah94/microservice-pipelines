5b25e12142485338e69da8019a8bb16e61ee741a
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2020-04-06 09:35:22 +0530

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -15,14 +15,14 @@ def call(){
 
 def packageArtifact(){
     stage("Package artifact") {
-        def mvnHome = "C:/Users/Aabha.Verma/Downloads/apache-maven-3.6.3-bin/apache-maven-3.6.3"
+        def mvnHome = "C:/Program Files (x86)/Jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven"
         bat "${mvnHome}/bin/mvn.cmd package"
     }
 }
 
 def buildAndTest(){
     stage("Backend tests"){
-        def mvnHome = "C:/Program Files (x86)/Jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven"
+        def mvnHome = "C:/Program Files (x86/Jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven"
         bat "mvn test"
     }
 }
