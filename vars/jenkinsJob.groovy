a348849ad04b2f83f9ffa83bb1f1f966b25e2083
Aabha <aabha.verma@infogain.com>
2020-04-07 15:45:35 +0530

    final

---
+++ vars/jenkinsJob.groovy
@@ -0,0 +1,32 @@
+def call(){
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+
+        // Execute different stages depending on the job
+		
+		packageArtifact()
+		test()
+		sonarScan()
+    }
+}
+
+def packageArtifact(){
+    stage("Package artifact") {
+        bat "mvn compile"
+        bat "mvn package"
+    }
+}
+
+def Test(){
+    stage("Backend tests"){
+        bat "mvn test"
+    }
+}
+	
+def sonarScan(){
+    stage("Sonar scan"){
+        bat "mvn sonar:sonar"
+    }
+}
